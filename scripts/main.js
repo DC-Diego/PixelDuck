e72fcc78563b7b68c288c782448c14f916c19d02
DC-Diego <dicavallaro.dc@gmail.com>
2026-02-23 16:17:02 -0300

    ADD: Código refatorado e corrigido

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,14 @@
 
 import {confirmDialog} from '../components/POPUP_confirm.js';
-import  '../scripts/splashScreen.js';
-
-
+import {splashScreen} from  '../scripts/splashScreen.js';
 
 function renderComponent(parent, child){
   parent.appendChild(child);
 
 }
 
-let i = 0;
-
-const confirmar = ()=>{
-  i++;
-  console.log(i);
-};
-
-const cancelar = ()=>{
-  i--;
-  console.log(i);
-};
+splashScreen();
 /*
 renderComponent(document.body, confirmDialog('Titulo lindo', 'Descrição Descrição Descrição Descrição', confirmar, cancelar));
-
 */
 // renderComponent(document.body, newFile());
--- scripts/splashScreen.js
+++ scripts/splashScreen.js
@@ -1,28 +1,30 @@
 import {newFile} from '../components/POPUP_newFile.js';
-const btnNewFile = document.getElementById("btnNewFile");
-
 
 function renderComponent(parent, child){
   parent.appendChild(child);
 }
 
-btnNewFile.addEventListener("click", ()=>{
-  const welcomeScreen = document.getElementById("welcomeScreen");
-  welcomeScreen.style.display = "none";
+function splashScreen(){
+  const btnNewFile = document.getElementById("btnNewFile");
+  const PAGEnewFile = document.getElementById("newFile");
   
-  const cancel = ()=>{
-    welcomeScreen.style.display = "flex";
 
-  }
-  const confirm = ()=>{
-    welcomeScreen.style.display = "flex";
+  btnNewFile.addEventListener("click", ()=>{
+    const welcomeScreen = document.getElementById("welcomeScreen");
+    welcomeScreen.style.display = "none";
+    
+    const cancel = ()=>{
+      welcomeScreen.style.display = "flex";
 
-  }
+    }
+    const confirm = ()=>{
+      welcomeScreen.style.display = "flex";
+
+    }
 
 
-  renderComponent(document.body, newFile(cancel,confirm));  
+    renderComponent(document.body, newFile(cancel,confirm));  
+  });
+}
 
-
-
-});
-
+export {splashScreen}
